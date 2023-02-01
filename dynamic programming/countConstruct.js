c887e9f914d5cda6a9b320791361573fb421832f
Irene-Mukii <renniequin@gmail.com>
2023-02-01 15:05:33 +0300

    passes last test

--- dynamic programming/countConstruct.js
+++ dynamic programming/countConstruct.js
@@ -1,14 +1,16 @@
 
-function countConstruct (target,wordBank){
+function countConstruct (target,wordBank,memo){
+    if(target in memo) return memo[target];
     if (target === '') return 1;
     let count =0;
     for (let word of wordBank){
         if(target.startsWith(word)){
             const newTarget = target.slice(word.length);
-            const numWaysForRest =  countConstruct(newTarget,wordBank);
+            const numWaysForRest =  countConstruct(newTarget,wordBank,memo);
             count += numWaysForRest;
         }
     }
+    memo[target] = count;
     return count;
 
 }
